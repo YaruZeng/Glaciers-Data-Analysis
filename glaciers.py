58b1949b8416cdf7ac80fe59d78205a49ac0e9f3
IvyZayn <kakapon713@outlook.com>
2021-11-18 11:57:21 +0000

    Initial documents.

---
+++ glaciers.py
@@ -0,0 +1,36 @@
+class Glacier:
+    def __init__(self, glacier_id, name, unit, lat, lon, code):
+        raise NotImplementedError
+
+    def add_mass_balance_measurement(self, year, mass_balance):
+        raise NotImplementedError
+
+    def plot_mass_balance(self, output_path):
+        raise NotImplementedError
+
+        
+class GlacierCollection:
+
+    def __init__(self, file_path):
+        raise NotImplementedError
+
+    def read_mass_balance_data(self, file_path):
+        raise NotImplementedError
+
+    def find_nearest(self, lat, lon, n):
+        """Get the n glaciers closest to the given coordinates."""
+        raise NotImplementedError
+    
+    def filter_by_code(self, code_pattern):
+        """Return the names of glaciers whose codes match the given pattern."""
+        raise NotImplementedError
+
+    def sort_by_latest_mass_balance(self, n, reverse):
+        """Return the N glaciers with the highest area accumulated in the last measurement."""
+        raise NotImplementedError
+
+    def summary(self):
+        raise NotImplementedError
+
+    def plot_extremes(self, output_path):
+        raise NotImplementedError
---
+++ utils.py
@@ -0,0 +1,6 @@
+def haversine_distance(lat1, lon1, lat2, lon2):
+    """Return the distance in km between two points around the Earth.
+
+    Latitude and longitude for each point are given in degrees.
+    """
+    raise NotImplementedError
